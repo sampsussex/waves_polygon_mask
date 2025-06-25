bf5bfc3e5e5e5339eed207be3e4ea35dc237714e
sp624AA <sp624AA@mps028555.mps.schools.susx.ac.uk&#8232>
2025-06-25 14:58:55 +0100

    Units in area

--- sum_area.py
+++ sum_area.py
@@ -7,7 +7,7 @@ df_n[0] = df_n[0].astype(float)
 total_area_n = df_n[0].sum()
 # convert to sq deg
 total_area_n = total_area_n*180**2/(np.pi**2)
-print(f"waves wide N total area: {total_area_n}")
+print(f"waves wide N total area: {total_area_n} sq deg")
 
 # Waves S
 df_s = pd.read_csv("mangle_masks/waves_wide_S_area.dat", delim_whitespace=True, skiprows=2, header=None)
@@ -15,14 +15,14 @@ df_s[0] = df_s[0].astype(float)
 total_area_s = df_s[0].sum()
 # convert to sq deg
 total_area_s = total_area_s*180**2/(np.pi**2)
-print(f"waves wide S total area: {total_area_s}")
+print(f"waves wide S total area: {total_area_s} sq deg")
 
 # Combined total
 total_area_combined = total_area_n + total_area_s
-print(f"waves wide combined total area: {total_area_combined}")
+print(f"waves wide combined total area: {total_area_combined} sq deg")
 
 # Save all results to a text file
 with open("waves_wide_total_area_full_mask.txt", "w") as f:
-    f.write(f"waves wide N total area: {total_area_n}\n")
-    f.write(f"waves wide S total area: {total_area_s}\n")
-    f.write(f"waves wide combined total area: {total_area_combined}\n")
+    f.write(f"waves wide N total area: {total_area_n} sq deg\n")
+    f.write(f"waves wide S total area: {total_area_s} sq deg\n")
+    f.write(f"waves wide combined total area: {total_area_combined} sq deg\n")
