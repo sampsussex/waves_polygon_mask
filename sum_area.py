32ed0a919ae9ec5b3e066d2155b1e5d00cee4cbf
sp624AA <sp624AA@mps028555.mps.schools.susx.ac.uk&#8232>
2025-06-25 14:51:25 +0100

    sq deg conversion

--- sum_area.py
+++ sum_area.py
@@ -1,15 +1,20 @@
 import pandas as pd
+import numpy as np
 
 # Waves N
-df_n = pd.read_csv("mangle_masks/waves_wide_N_area.dat", delim_whitespace=True, skiprows=1, header=None)
+df_n = pd.read_csv("mangle_masks/waves_wide_N_area.dat", delim_whitespace=True, skiprows=2, header=None)
 df_n[0] = df_n[0].astype(float)
 total_area_n = df_n[0].sum()
+# convert to sq deg
+total_area_n = total_area_n*180**2/(np.pi**2)
 print(f"waves wide N total area: {total_area_n}")
 
 # Waves S
-df_s = pd.read_csv("mangle_masks/waves_wide_S_area.dat", delim_whitespace=True, skiprows=1, header=None)
+df_s = pd.read_csv("mangle_masks/waves_wide_S_area.dat", delim_whitespace=True, skiprows=2, header=None)
 df_s[0] = df_s[0].astype(float)
 total_area_s = df_s[0].sum()
+# convert to sq deg
+total_area_s = total_area_s*180**2/(np.pi**2)
 print(f"waves wide S total area: {total_area_s}")
 
 # Combined total
