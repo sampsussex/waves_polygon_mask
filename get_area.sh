a15ca5f991c3ec4389951ef6e7226bbc37f30135
sp624AA <sp624AA@mps028555.mps.schools.susx.ac.uk&#8232>
2025-06-25 14:18:35 +0100

    added a function to find the area of each mask

---
+++ get_area.sh
@@ -0,0 +1,9 @@
+# Point to mangle directory
+export MANGLE_DIR="/research/astro/gama/loveday/sw/mangle2.2"
+
+$MANGLE_DIR/bin/poly2poly -oa waves_wide_N_full_mask.ply mangle_masks/waves_wide_N_area.dat
+$MANGLE_DIR/bin/poly2poly -oa waves_wide_S_full_mask.ply mangle_masks/waves_wide_S_area.dat
+
+
+#$MANGLE_DIR/bin/poly2poly -oa final_masks/waves_wide_N_full_mask_old_radius_rule.ply mangle_masks/waves_wide_N_area.dat
+#$MANGLE_DIR/bin/poly2poly -oa final_masks/waves_wide_S_full_mask_old_radius_rule.ply mangle_masks/waves_wide_S_area.dat
