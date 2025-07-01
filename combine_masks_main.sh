0ede518d69bb753017bf2e66c2a23244b900e3ce
Sam Philipsborn <89216488+sampsussex@users.noreply.github.com>
2025-07-01 16:33:09 +0100

    Update combine_masks_main.sh
    Temp hashed out n for s update

--- combine_masks_main.sh
+++ combine_masks_main.sh
@@ -3,17 +3,17 @@ export MANGLE_DIR="/research/astro/gama/loveday/sw/mangle2.2"
 
 
 # Combine (zero-weight) masks with (unit-weight) waves region
-$MANGLE_DIR/bin/pixelize mangle_masks/waves_n_rect.ply mangle_masks/starmask_n_reg.ply mangle_masks/ngc_n_reg.ply mangle_masks/ghostmask_n_reg.ply temp_n_1
+#$MANGLE_DIR/bin/pixelize mangle_masks/waves_n_rect.ply mangle_masks/starmask_n_reg.ply mangle_masks/ngc_n_reg.ply mangle_masks/ghostmask_n_reg.ply temp_n_1
 $MANGLE_DIR/bin/pixelize mangle_masks/waves_s_rect.ply mangle_masks/starmask_s_reg.ply mangle_masks/ngc_s_reg.ply mangle_masks/extra_source_s_reg.ply mangle_masks/ghostmask_s_reg.ply temp_s_1
 
 # Snap, balkanize & unify
-$MANGLE_DIR/bin/snap temp_n_1 temp_n_2
+#$MANGLE_DIR/bin/snap temp_n_1 temp_n_2
 $MANGLE_DIR/bin/snap temp_s_1 temp_s_2
 
-$MANGLE_DIR/bin/balkanize temp_n_2 temp_n_3
+#$MANGLE_DIR/bin/balkanize temp_n_2 temp_n_3
 $MANGLE_DIR/bin/balkanize temp_s_2 temp_s_3
 
-$MANGLE_DIR/bin/unify temp_n_3 waves_wide_N_full_mask.ply
+#$MANGLE_DIR/bin/unify temp_n_3 waves_wide_N_full_mask.ply
 $MANGLE_DIR/bin/unify temp_s_3 waves_wide_S_full_mask.ply
 
 #Generate 1000000 randoms. 
